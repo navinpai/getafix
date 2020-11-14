d3c3df9b271022ac09a65e5b5f1eebc7c9ac69cc
Naveen Pai <naveen.pai@tact.ai>
2020-11-15 00:52:48 +0530

    Adding notes for Section 2

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -4,6 +4,9 @@ defmodule Cards do
   """
   # Comments in Elixir are written with #
 
+  @doc"""
+    Creates a deck
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Clubs", "Hearts", "Diamonds", "Spades"]
@@ -19,6 +22,26 @@ defmodule Cards do
   
   def contains?(deck, card) do
     Enum.member?(deck, card) 
+  end
+  
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+  
+  def load(filename) do
+    case File.read(filename) do
+      # When in doubt, you can treat atoms like strings
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _} -> "The file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    # Pipe Operator ALWAYS sets the output as first parameter of next method
+    Cards.create_deck
+    |> shuffle
+    |> deal(hand_size)
   end
   
   def deal(deck, hand_size) do
--- cards/mix.exs
+++ cards/mix.exs
@@ -21,6 +21,8 @@ defmodule Cards.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # To create docs, run `mix docs` to create epub and html docs
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
